d6a84b405fe9c7bf3f434b4487edc8fda8d0158c
jerluc <jeremyalucas@gmail.com>
2017-01-26 23:32:38 -0800

    Complete Go implementation

---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+    "log"
+	"net"
+	"os"
+	"os/exec"
+	"github.com/jerluc/gobee"
+	"github.com/jerluc/serial"
+	"github.com/songgao/water"
+	"golang.org/x/net/ipv6"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+const (
+	DefaultInterface = "rift0"
+	DefaultLocalIP = "2001:412:abcd:1::"
+)
+
+var (
+	riftd = kingpin.New("riftd", "Rift protocol daemon")
+	ifaceName = riftd.Flag("iface", "Network interface name").Default(DefaultInterface).String()
+	devname = riftd.Flag("dev", "Serial device name").OverrideDefaultFromEnvar("DEVNAME").Required().String()
+	cidr = riftd.Flag("cidr", "IPv6 64-bit prefix").Default(DefaultLocalIP).IP()
+)
+
+func packBytes(bas... []byte) []byte {
+	var packed []byte
+	for _, ba := range bas {
+		packed = append(packed, ba...)
+	}
+	return packed
+}
+
+func incoming(cidr net.IP, inbox <-chan gobee.Frame, iface *water.Interface) {
+	for {
+		packet := (<-inbox).(*gobee.Rx64Frame)
+		log.Println("Incoming packet:", packet)
+		srcPort := gobee.BytesToUint16(packet.Data[:2])
+		dstPort := gobee.BytesToUint16(packet.Data[2:4])
+		payload := packet.Data[4:]
+		srcIP := append([]byte(cidr[:8]), packet.Source...)
+
+		srcAddr := &net.UDPAddr{
+			IP: srcIP,
+			Port: int(srcPort),
+		}
+
+		dstAddr := &net.UDPAddr{
+			// Loopback address
+			IP: []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			Port: int(dstPort),
+		}
+
+		log.Println("src =", srcAddr, "dst =", dstAddr, "payload =", string(payload))
+
+		rawPkt := BuildRawPacket(srcAddr, dstAddr, payload)
+		err := SendRawPacket(rawPkt)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func isOutgoingPacket(header *ipv6.Header) bool {
+	for i, b := range header.Dst[:4] {
+		if b != header.Src[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func tun(ifaceName string) *water.Interface {
+    iface, err := water.NewTUN(ifaceName)
+    if err != nil {
+        panic(err)
+    }
+	return iface
+}
+
+func devUp(ifaceName string, cidr net.IP) {
+	err := exec.Command("ip", "addr", "add", cidr.String() + "/64", "dev", ifaceName).Run()
+	if err != nil {
+		panic(err)
+	}
+
+	err = exec.Command("ip", "link", "set", ifaceName, "up").Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func outgoing(iface *water.Interface, outbox chan<- gobee.Frame) {
+    for {
+		// TODO: Is this okay?
+		packet := make([]byte, 127)
+        n, err := iface.Read(packet)
+		if err != nil {
+            panic(err)
+        }
+		log.Println("Received IPv6 packet: % x", packet[:n])
+		header, err := ipv6.ParseHeader(packet[:n])
+		if err != nil {
+			panic(err)
+		}
+
+		if isOutgoingPacket(header) {
+			// IPv6 payload starts after 40 byte fixed-size header
+			srcPort := packet[40:42]
+			dstPort := packet[42:44]
+			dst := header.Dst[8:]
+			payload := packet[48:]
+			packet := &gobee.Tx64Frame{
+				ID: 0x00,
+				Destination: dst,
+				Options: 0x00,
+				Data: gobee.PackBytes(srcPort, dstPort, payload),
+			}
+			log.Println("Outgoing packet:", packet)
+			outbox <- packet
+		}
+    }
+}
+
+func main() {
+	kingpin.MustParse(riftd.Parse(os.Args[1:]))
+
+	serialCfg := &serial.Config{Name: *devname, Baud: 9600}
+	serialPort, openErr := serial.OpenPort(serialCfg)
+	if openErr != nil {
+		panic(openErr)
+	}
+
+	iface := tun(*ifaceName)
+	devUp(*ifaceName, *cidr)
+
+	mailbox := gobee.NewMailbox(serialPort)
+	inbox, outbox := mailbox.Inbox(), mailbox.Outbox()
+
+	go incoming(*cidr, inbox, iface)
+	go outgoing(iface, outbox)
+
+	defer func() {
+		iface.Close()
+		close(outbox)
+		serialPort.Close()
+	}()
+	select{}
+}
---
+++ raw.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"net"
+	"syscall"
+)
+
+func computeUDPChecksum(buf []byte) uint16 {
+	sum := uint32(0)
+
+	for ; len(buf) >= 2; buf = buf[2:] {
+		sum += uint32(buf[0])<<8 | uint32(buf[1])
+	}
+	if len(buf) > 0 {
+		sum += uint32(buf[0]) << 8
+	}
+	for sum > 0xffff {
+		sum = (sum >> 16) + (sum & 0xffff)
+	}
+	csum := ^uint16(sum)
+	/*
+	 * From RFC 768:
+	 * If the computed checksum is zero, it is transmitted as all ones (the
+	 * equivalent in one's complement arithmetic). An all zero transmitted
+	 * checksum value means that the transmitter generated no checksum (for
+	 * debugging or for higher level protocols that don't care).
+	 */
+	if csum == 0 {
+		csum = 0xffff
+	}
+	return csum
+}
+
+func BuildRawPacket(srcAddr *net.UDPAddr, dstAddr *net.UDPAddr, payload []byte) []byte {
+	buf := bytes.NewBuffer([]byte{})
+	// Source address
+	binary.Write(buf, binary.BigEndian, srcAddr.IP)
+	// Destination address
+	binary.Write(buf, binary.BigEndian, dstAddr.IP)
+	// Length (extra 8 bytes for UDP header)
+	binary.Write(buf, binary.BigEndian, uint16(len(payload) + 8))
+	// 12-byte zero padding
+	binary.Write(buf, binary.BigEndian, uint8(0))
+	binary.Write(buf, binary.BigEndian, uint8(0))
+	binary.Write(buf, binary.BigEndian, uint8(0))
+	// Next header (17 = UDP!)
+	binary.Write(buf, binary.BigEndian, byte(17))
+	// Source port
+	binary.Write(buf, binary.BigEndian, uint16(srcAddr.Port))
+	// Destination port
+	binary.Write(buf, binary.BigEndian, uint16(dstAddr.Port))
+	// Length (extra 8 bytes for UDP header)
+	binary.Write(buf, binary.BigEndian, uint16(len(payload) + 8))
+	// Checksum placeholder
+	binary.Write(buf, binary.BigEndian, uint16(0))
+	// Data payload
+	binary.Write(buf, binary.BigEndian, payload)
+
+	checksum := computeUDPChecksum(buf.Bytes())
+
+	buf.Reset()
+
+	// Version, traffic class, flow label
+	binary.Write(buf, binary.BigEndian, byte(96))
+	binary.Write(buf, binary.BigEndian, byte(0))
+	binary.Write(buf, binary.BigEndian, byte(0))
+	binary.Write(buf, binary.BigEndian, byte(0))
+	// Length (extra 8 bytes for UDP header)
+	binary.Write(buf, binary.BigEndian, uint16(len(payload) + 8))
+	// Next header (17 = UDP!)
+	binary.Write(buf, binary.BigEndian, byte(17))
+	// Hop limit
+	binary.Write(buf, binary.BigEndian, byte(64))
+	// Source address
+	binary.Write(buf, binary.BigEndian, srcAddr.IP)
+	// Destination address
+	binary.Write(buf, binary.BigEndian, dstAddr.IP)
+
+	// Source port
+	binary.Write(buf, binary.BigEndian, uint16(srcAddr.Port))
+	// Destination port
+	binary.Write(buf, binary.BigEndian, uint16(dstAddr.Port))
+	// Length (extra 8 bytes for UDP header)
+	binary.Write(buf, binary.BigEndian, uint16(len(payload) + 8))
+	// Checksum placeholder
+	binary.Write(buf, binary.BigEndian, uint16(checksum))
+	// Data payload
+	binary.Write(buf, binary.BigEndian, payload)
+
+	return buf.Bytes()
+}
+
+func SendRawPacket(packet []byte) error {
+	fd, err := syscall.Socket(syscall.AF_INET6, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
+	if err != nil {
+		return err
+	}
+	defer syscall.Close(fd)
+
+	return syscall.Sendto(fd, packet, 0, &syscall.SockaddrInet6{})
+}
